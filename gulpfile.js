6a5ffceaa018f3aa4f064aa9ef760f49d6343d94
Barry Buitelaar <barry@infoprojects.nl>
2017-10-06 15:33:16 +0200

    Pug installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,36 @@
-var gulp = require('gulp'),
-path = require('path'),
-sass = require('gulp-sass'),
-config = require('./config.json');
+var gulp = require("gulp"),
+  path = require("path"),
+  sass = require("gulp-sass"),
+  config = require("./config.json");
 
 function resolvePath(pathInput) {
-return path.resolve(pathInput).replace(/\\/g, '/');
+  return path.resolve(pathInput).replace(/\\/g, "/");
 }
 
-gulp.task('build-sass', function () {
-return (gulp
-  .src('**/*.scss', { cwd: resolvePath(paths().source.scss) })
-  .pipe(
-    sass({
-      includePaths: ['node_modules/susy/sass']
-    }))
-  .pipe(gulp.dest(paths().dest.css))
-);
+gulp.task("build-sass", function() {
+  return gulp
+    .src("**/*.scss", { cwd: resolvePath(paths().source.scss) })
+    .pipe(
+      sass({
+        includePaths: ["node_modules/susy/sass"]
+      })
+    )
+    .pipe(gulp.dest(paths().dest.css));
 });
 
 function paths() {
-return config.paths;
+  return config.paths;
 }
 
-var pug = require('gulp-pug');
+var pug = require("gulp-pug");
 
-gulp.task('views', function buildHTML() {
-return gulp.src('./views/*.pug')
-.pipe(pug({
-pretty: true
-}))
-.pipe(gulp.dest('./html'));
+gulp.task("views", function buildHTML() {
+  return gulp
+    .src("./views/*.pug")
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
+    .pipe(gulp.dest("./html"));
 });
-
