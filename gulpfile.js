02edfafbec94b74ca283a04b3b5e5f33086ce29e
Barry Buitelaar <barrybuitelaar@gmail.com>
2017-11-02 13:58:00 +0100

    IPBCB-5 and IPBCB-8 (#10)
    IPBCB-5 and IPBCB-8

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,45 @@
-var gulp = require("gulp"),
-  sass = require("gulp-sass"),
-  pug = require("gulp-pug");
-  config = require("./config.json"),
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  pug = require('gulp-pug'),
+  config = require('./config.json'),
+  browserSync = require('browser-sync').create(),
   paths = config.paths;
 
-gulp.task('default', ['build-sass','views']);
+gulp.task('default', ['build-sass', 'build-views', 'serve', 'watch']);
 
-gulp.task("build-sass", function() {
+gulp.task('watch', function(){
+  gulp.watch('./scss/**/*.scss', ['build-sass']);
+  gulp.watch('./views/*.pug', ['build-views']);
+});
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './public',
+      directory: true
+    },
+    files: ['./public/**/*']
+  });
+});
+
+gulp.task('build-sass', function() {
   return gulp
-    .src("*.scss", { cwd: (paths.source.scss) })
+    .src('*.scss', { cwd: (paths.source.scss) })
     .pipe(
       sass({
-        includePaths: ["node_modules/susy/sass"]
+        includePaths: ['node_modules/susy/sass']
       })
     )
     .pipe(gulp.dest(paths.dest.css));
 });
 
-gulp.task("views", function buildHTML() {
+gulp.task('build-views', function buildHTML() {
   return gulp
-    .src("./views/*.pug")
+    .src('./views/*.pug')
     .pipe(
       pug({
         pretty: true
       })
     )
-    .pipe(gulp.dest("./html"));
+    .pipe(gulp.dest('./public/html'));
 });
